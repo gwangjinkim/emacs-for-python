7f2b7a4c18724010d7f7a43ea698c98ff09e4761
gwangjinkim <gwang.jin.kim.phd@gmail.com>
2024-09-08 09:25:28 +0200

    add dape

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -6,10 +6,10 @@
 ;; to put infront package-initialize ;; works!
 (setq package-enable-at-startup nil)
 (setq package-archives '())
-;; (add-to-list 'package-archives '("elpa"      . "http://elpa.gnu.org/packages") t)
+(add-to-list 'package-archives '("elpa"      . "http://elpa.gnu.org/packages/") t)
 ;; (add-to-list 'package-archives '("elpa" . "http://tromey.com/elpa/") t)
 (add-to-list 'package-archives '("melpa"     . "http://melpa.org/packages/") t)
-;; (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
 ;; M-x package-refresh-contents
 (package-initialize)
 (package-refresh-contents)
